5562a0f1a57472f665853a048ad56aca016e2e73
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-24 03:53:51 +0100

    alignement des planètes à la taille et  mouvement

---
+++ systeme.js
@@ -0,0 +1,13 @@
+const planets = document.querySelectorAll('.planet')
+const p_radii = [22,33,50,70,112,138,165,190]
+let p_radians = new Array(8).fill(0)
+const p_velocities = [1.607,1.174,1,0.802,0.434,0.323,0.228,0.182]
+//il y a 8 planètes autour du soleil donc 8 vitesses et 8 angles.
+
+setInterval(()=>{
+    planets.forEach((planet,index)=>{
+        planet.style.left=`${Math.cos(p_radians[index])*p_radii[index]}vmin`
+        planet.style.top=`${Math.sin(p_radians[index])*p_radii[index]}vmin`
+        p_radians[index] += p_velocities[index]
+    })
+},1000/60)
