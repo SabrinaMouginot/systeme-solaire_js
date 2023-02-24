96bb2b692b5ba980afc69bda5e1dc74aedb222fb
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-24 07:25:31 +0100

    ajout de la Lune

--- systeme.js
+++ systeme.js
@@ -4,6 +4,11 @@ let p_radians = new Array(8).fill(0)
 const p_velocities = [1.607,1.174,1,0.802,0.434,0.323,0.228,0.182]
 //il y a 8 planètes autour du soleil donc 8 vitesses et 8 angles.
 
+const moon = document.querySelector('#moon')
+const m_radius = 8
+let m_radians = 0
+const m_velocity = 10
+
 const p_orbits = document.querySelectorAll('.p-orbit')
 
 p_orbits.forEach((p_orbit,index)=>{
@@ -17,4 +22,16 @@ setInterval(()=>{
         planet.style.top=`${Math.sin(p_radians[index])*p_radii[index]}vmin`
         p_radians[index] += p_velocities[index]*0.02
     })
+
+    moon.style.left=`${earthX() + (Math.cos(m_radians)*m_radius)}vmin`
+    moon.style.top=`${earthY() + (Math.sin(m_radians)*m_radius)}vmin`
+    // m_radians += m_velocity*0.02    
 },1000/60)
+
+function earthX(){
+    return Number(planets[2].style.left.split('vmin')[0])
+}
+
+function earthY(){
+    return Number(planets[2].style.top.split('vmin')[0])
+}
